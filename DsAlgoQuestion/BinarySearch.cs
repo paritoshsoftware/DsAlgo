38af8a2eb9ed1336ba86f760c41ae339ffe304cb
PARITOSH SUDAME <sudame.paritosh@gmail.com>
2020-09-29 21:07:24 +0530

    Initial Commit

---
+++ DsAlgoQuestion/BinarySearch.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DsAlgoQuestion
+{
+	public class BinarySearchProgram
+	{
+
+		//Write a function that takes in a sorted array of integers as well as a target
+		//integer.The function should use the Binary Search algorithm to determine if
+		//the target integer is contained in the array and should return its index if it
+		//is, otherwise
+		public static int BinarySearch(int[] array, int target)
+		{
+        	int left = 0;
+			int right = array.Length - 1;
+			int mid = 0;
+			bool isFound = false;
+			while (left <= right)
+			{
+				mid = (left + right) / 2;
+				if (target == array[mid])
+				{
+					isFound = true;
+					break;
+				}
+				if (target > array[mid])
+					left = mid + 1;
+				else
+					right = mid - 1;
+			}
+
+			if (isFound == true)
+				return mid;
+			else
+				return -1;
+		}
+		
+	}
+}
---
+++ DsAlgoQuestion/MainDriverCode.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DsAlgoQuestion
+{
+    class MainDriverCode
+    {
+        static void Main(string[] args)
+        {
+           
+            int[] arr = { 0, 1, 21, 33, 45, 45, 61, 71, 72, 73 };
+            int res = BinarySearchProgram.BinarySearch(arr,45);
+            Console.WriteLine(res);
+             Console.ReadLine();
+        }
+    }
+}
