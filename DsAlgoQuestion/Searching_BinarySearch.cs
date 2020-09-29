aff290c7c9ad855c732c89f1467b75ade26168d0
PARITOSH SUDAME <sudame.paritosh@gmail.com>
2020-09-29 21:15:28 +0530

    Initial Commit

--- DsAlgoQuestion/Searching_BinarySearch.cs
+++ DsAlgoQuestion/Searching_BinarySearch.cs
@@ -10,7 +10,7 @@ namespace DsAlgoQuestion
 		//Write a function that takes in a sorted array of integers as well as a target
 		//integer.The function should use the Binary Search algorithm to determine if
 		//the target integer is contained in the array and should return its index if it
-		//is, otherwise
+		//is, otherwise return -1
 		public static int BinarySearch(int[] array, int target)
 		{
         	int left = 0;
